29eae96da523a340fe8aa424444976e46cd78d2a
Friederike Schueuer <Friederike.schueuer@gmail.com>
2014-08-05 18:58:24 -0400

    extract per year from command line

--- code/fda_accessAPI.py
+++ code/fda_accessAPI.py
@@ -1,166 +1,79 @@
 #--------------------------------------------
-# https://open.fda.gov/drug/event/reference/#data-downloads
-# http://www.fda.gov/Drugs/InformationOnDrugs/ucm079750.htm [list of FDA approved drugs and notes on approval process]
-# http://www.fda.gov/drugs/informationondrugs/ucm129689.htm
-# http://www.medilexicon.com/drugsearch.php?z=true
-# http://www.centerwatch.com/drug-information/fda-approved-drugs/drug-names/A # can be used for known vs. novel side effects
-# http://www.ncbi.nlm.nih.gov/pubmed/16136624
+# README: this file accesses the FDA API for a year determined by user input (e.g. 2013) and saves the data as a JSON file
 #--------------------------------------------
 
+#--------------------------------------------
+# modules
+#--------------------------------------------
 import requests
 import json
 import pandas as pd
 import os
 import sys
+import argparse
 
+#--------------------------------------------
 # parameters
-save = None
-
-# load list of all FDA approved drugs (flat file)
-filename = 'fda_allApproved/Product.txt'
-appDrugs = pd.read_table(filename,delimiter='\t')
-print 'FDA approved a total of ' + str(len(appDrugs)) + ' drugs.\n'
-
-# interacting with API
+#--------------------------------------------
+save = 1
 myKey = 'R4RFOXASGiQNvdPPD7XHSRZ0jGTg7xVrhjdtpa1N'
 root = 'https://api.fda.gov'
 apiDrugs = '/drug/event' # drug event endpoint
 apiFormat = '.json?' # results returns in json format
 
-# limit query to past 6 months
-query = '&search=receivedate:[20130601+TO+20150101]'
+# path names
+location_code = '/Users/Friederike/Dropbox/Insight/Project/FDA/code'
+location_data = '/Users/Friederike/Dropbox/Insight/Project/FDA/data/FDA_jsons'
 
-a = 99
-b = 0
-results = []
-while True:
-  limits = '&limit='+str(a)+'&skip='+str(b)
-  url = root+apiDrugs+apiFormat+'api_key='+myKey+query+limits
+#--------------------------------------------
+# access API & return JSON
+#--------------------------------------------
 
-  print 'Accessing: ' + query+limits + '\n'
-  page = requests.get(url)
-  page = page.json()
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(description = 'Enter the year of FDA data for download (2004 till present)')
+  parser.add_argument("dataToDownload", help = "Enter what year of FDA data to download.", type = str)
+  args = parser.parse_args()
+  year = args.dataToDownload
   
-  try:
-    for r in page['results']:
-      results.append(r)
-    b = b+99
-  except KeyError:
-    print page['error']['message']
-    break
+  yearEnd = str(int(year)+1)
+  time_span = '['+year+'0101+TO+'+yearEnd+'0101]'
+  query = '&search=receivedate:' + time_span
 
-print 'Records retrieved: ' + str(len(results)) + '\n'
+  a = 99 # number of entries to receive (API only allows queries that return entries < 100)
+  b = 0  # number of entries to skip
 
-# save JSON for later use if save
-if save:
-  filename = 'FDA_20130601-20150101.json'
-  with open(filename, "w") as outfile:
-      json.dump(results, outfile, sort_keys=True, indent=2)
+  results = []
+  while b<= 1000:
 
-data = {}
-howMany = []
-for r in results:
+    limits = '&limit='+str(a)+'&skip='+str(b)
+    url = root+apiDrugs+apiFormat+'api_key='+myKey+query+limits
 
-  howMany.append(len(r['patient']['drug']))
+    print 'Accessing: ' + query+limits + '\n'
+    page = requests.get(url)
+    page = page.json()
 
-  # only include patients who take one drug
-  if len(r['patient']['drug']) == 1:
-    
-    if 'id' not in data:
-      data['id'] = []
-    data['id'].append(r['safetyreportid'])
-  
-    if 'drugIndication' not in data:
-      data['drugIndication'] = []
     try:
-      data['drugIndication'].append(r['patient']['drug'][0]['drugindication'])
+      totalRecords = page['meta']['results']['total']
     except KeyError:
-      data['drugIndication'].append(None)
+      totalRecords = -1
 
-    if 'serious' not in data:
-      data['serious'] = []
     try:
-      data['serious'].append(int(r['serious']))
+      for r in page['results']:
+        results.append(r)
+      b = b+99
     except KeyError:
-      data['serious'].append(None)
+      print page['error']['message']
+      break
 
-    if 'weight' not in data:
-      data['weight'] = []
-    try:
-      data['weight'].append(float(r['patient']['patientweight']))
-    except KeyError:
-      data['weight'].append(None)
+  print 'Records retrieved: ' + str(len(results)) + ' out of ' + str(totalRecords) + '\n'
 
-    if 'sex' not in data:
-      data['sex'] = []
-    try:
-      data['sex'].append(int(r['patient']['patientsex']))
-    except KeyError:
-      data['sex'].append(None)
-    
-    if 'age' not in data:
-      data['age'] = []
-    try:
-      data['age'].append(float(r['patient']['patientonsetage']))
-    except KeyError:
-      data['age'].append(None)
-
-    if 'outcome' not in data:
-      data['outcome'] = []
-    try:
-      data['outcome'].append(int(r['patient']['reaction'][0]['reactionoutcome']))
-    except KeyError:
-      data['outcome'].append(None)
-
-    if 'reaction' not in data:
-      data['reaction'] = []
-    try:
-      data['reaction'].append(r['patient']['reaction'][0]['reactionmeddrapt'])
-    except KeyError:
-      data['reaction'].append(None)
-    
-df = pd.DataFrame(data,index=data['id'])
-
-gb = df.groupby(['sex','serious'])
-gb.median() # females overrepresented compared to population, females younger, age gap
-
-# most common reactions
-yvals = df.reaction.value_counts()[0:6]
-xTick = tuple(df.reaction.value_counts().index[0:6])
-xvals = range(0,6)
-barGraph(xvals,yvals,1,xTick,'adverse reaction','counts',0.5,'adverseReaction.jpeg')
-
-# most common reactions
-yvals = df.drugIndication.value_counts()[0:6]
-xTick = df.drugIndication.value_counts().index[0:6]
-xvals = range(0,6)
-barGraph(xvals,yvals,1,xTick,'cause for taking medication','counts',0.5,'reasonForTakingMed.jpeg')
-
-def barGraph(xvals,yvals,width,xTick,xLabel,yLabel,bottomAdjust,saveFigure=0):
-  fig, ax = plt.subplots()
-  rects = ax.bar(xvals,yvals,width,color='r')
-  ax.set_ylabel(yLabel)
-  ax.set_title(xLabel)
-  ax.set_xticks(xvals)
-  ax.set_xticklabels(xTick)
-  for tick in ax.xaxis.get_major_ticks():
-      tick.label.set_fontsize(8) 
-      tick.label.set_rotation('vertical')
-  if bottomAdjust != 0:
-    fig.subplots_adjust(bottom=bottomAdjust)
-  plt.show()
-  if isinstance(saveFigure,str):
-    fig.set_size_inches(7,5)
-    plt.savefig(saveFigure,dpi=100)
-
-
-url = 'https://api.fda.gov/drug/event.json?search=receivedate:[20040101+TO+20150101]+AND+patient.drug.openfda.substance_name:FESOTERODINE+FUMARATE&count=receivedate'
-page = requests.get(url)
-page = page.json()
-
-counts = []
-for r in page['results']:
-  counts.append(r['count'])
-
+  # save JSON for later use if save
+  if save:
+    os.chdir(location_data)
+    filename = 'FDA_'+year+'.json'
+    with open(filename, "w") as outfile:
+        json.dump(results, outfile, sort_keys=True, indent=2)
+    os.chdir(location_code)
 
 
