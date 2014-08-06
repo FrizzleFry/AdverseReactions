ce947de4d3a0e298a53f60c37b6fbceb0b00a77b
Friederike Schueuer <Friederike.schueuer@gmail.com>
2014-08-05 22:32:56 -0400

    add sql queries

--- code/umls_to_mysql.py
+++ code/umls_to_mysql.py
@@ -14,11 +14,25 @@ from umls_to_mysql_queries import createTable
 #------------------------------------------------
 # parameters
 #------------------------------------------------
-to_load = 100 # how many rows to load (for prototyping)
+to_load = 1000 # how many rows to load (for prototyping)
 
 # path names
 location_code = '/Users/Friederike/Dropbox/Insight/Project/FDA/code'
 location_data = '/Users/Friederike/Dropbox/Insight/Project/FDA/data/RxNorm_full_08042014/rrf'
+
+#------------------------------------------------
+# functions
+#------------------------------------------------
+
+# determine "length" of VARCHAR
+def MySqlTable_DataTypes(entries, name):
+  maxLen = 1
+  for i in entries:
+    if len(str(i))> maxLen:
+      maxLen = len(str(i))
+  maxLen += 0.2*maxLen
+  maxLen = int(maxLen)
+  return name + ' varchar('+str(maxLen)+')'
 
 #------------------------------------------------
 # loading, processing, and inserting data into database
@@ -42,17 +56,19 @@ for file in files[1:2]:
   
   # load
   os.chdir(location_data)
-  data = pd.read_table(file,sep='|',nrows = to_load, header = None)
+  data = pd.read_table(file,sep='|', nrows = to_load, header = None)
   os.chdir(location_code)
 
   # pre-process
   data.columns = headers
   data = data.dropna(axis=1)
-  headers = list(headers) # update (without empty columns)
+  headers = list(data.columns) # update (without empty columns)
+
+  # database
+  tableName = file.split('.')[0].lower()    
 
   # create connection to db
   con = mdb.connect('localhost', 'root', '', 'RxNorm'); #host, user, password, #database
-  tableName = file.split('.')[0].lower()
 
   # insert data
   with con:
@@ -62,27 +78,23 @@ for file in files[1:2]:
     query = """DROP TABLE IF EXISTS %s;""" %tableName
     cur.execute(query)
 
-    # use headers, determine value type from file or manually ...
-    # one could write a script that checks for value type per column starting from smaller storage to larger
-    # this script I could reuse when later building other databases ...
-    # then construct the query string ...
-    if file == 'RXNDOC.RRF':
-      value_types = ['INT']
-    elif file == 'RXNCONSO.RRF':
-      value_types = []
-    elif file == 'RXNREL.RRF':
-      value_types = []
-    else:
-      print "No appropriate value types found ..."
-
-    # create table - rethink the values for each column in database - use datetime and integers
-    query = createTable(tableName)
+    # qTypes = '"""CREATE TABLE %s (' %tableName
+    # for h in headers:
+    #   valueType = MySqlTable_DataTypes(data[h], h)
+    #   qTypes = qTypes + valueType
+    #   if h != headers[-1]: 
+    #     qTypes = qTypes+', '
+    #   else:
+    #     qTypes = qTypes+');"""'
+    
+    query = """CREATE TABLE rxnconso
+    (rxcui varchar(8) NOT NULL, language varchar(3) NOT NULL, rxaui varchar(8) NOT NULL, source_abbr varchar(20) NOT NULL, source_term_type varchar(20) NOT NULL, most_useful_id varchar(50), string varchar(3000), suppress_view varchar(1));"""
     cur.execute(query)
     
     # insert values for every row in the file - not necessary to specify column names, so just use row by row value ...
     for i in range(0,len(data)):
       values = tuple(data.iloc[i])
-      query = insertTable(tableName,values) # function definition to be written
+      query = """INSERT INTO rxnconso VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');""" %values
       cur.execute(query)
 
 
